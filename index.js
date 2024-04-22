83600f5df0b0e2a994f7a044576937387d456639
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-04-22 16:30:52 +0200

    added eventListeners for the sidebar

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // TASK: import helper functions from utils
+
 // TASK: import initialData
 
 
@@ -85,7 +86,7 @@ function filterAndDisplayTasksByBoard(boardName) {
       taskElement.setAttribute('data-task-id', task.id);
 
       // Listen for a click event on each task and open a modal
-      taskElement.click() => { 
+      taskElement.click() ; { 
         openEditTaskModal(task);
       });
 
@@ -142,7 +143,7 @@ function addTaskToUI(task) {
 function setupEventListeners() {
   // Cancel editing task event listener
   const cancelEditBtn = document.getElementById('cancel-edit-btn');
-  cancelEditBtn.click() => toggleModal(false, elements.editTaskModal));
+  cancelEditBtn.addEventListener('click' , () => toggleModal(false, elements.editTaskModal));
 
   // Cancel adding new task event listener
   const cancelAddTaskBtn = document.getElementById('cancel-add-task-btn');
@@ -158,8 +159,8 @@ function setupEventListeners() {
   });
 
   // Show sidebar event listener
-  elements.hideSideBarBtn.click() => toggleSidebar(false));
-  elements.showSideBarBtn.click() => toggleSidebar(true));
+  elements.hideSideBarBtn.addEventListener('click' ,() => toggleSidebar(false));
+  elements.showSideBarBtn.addEventListener('click' ,() => toggleSidebar(true));
 
   // Theme switch event listener
   elements.themeSwitch.addEventListener('change', toggleTheme);
